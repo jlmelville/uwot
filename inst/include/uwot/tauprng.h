3897feef9c059603f4cf49cbf85d664dee986312
James Melville <jlmelville@gmail.com>
2021-09-12 18:16:21 -0700

    use brace initializer.

--- inst/include/uwot/tauprng.h
+++ inst/include/uwot/tauprng.h
@@ -36,9 +36,9 @@ struct tau_prng {
   uint64_t state1; // technically this needs to always be > 7
   uint64_t state2; // and this should be > 15
 
-  static constexpr uint64_t MAGIC0 = static_cast<uint64_t>(4294967294);
-  static constexpr uint64_t MAGIC1 = static_cast<uint64_t>(4294967288);
-  static constexpr uint64_t MAGIC2 = static_cast<uint64_t>(4294967280);
+  static constexpr uint64_t MAGIC0{4294967294};
+  static constexpr uint64_t MAGIC1{4294967288};
+  static constexpr uint64_t MAGIC2{4294967280};
 
   tau_prng(uint64_t state0, uint64_t state1, uint64_t state2)
       : state0(state0), state1(state1 > 7 ? state1 : 8),
